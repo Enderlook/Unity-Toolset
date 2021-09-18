bbd9cc3ea4798a79a6b6ca589e83ab24c36b3da2
Enderlook <enderlook@gmail.com>
2021-09-18 15:31:53 -0300

    Fix CannotBeUsedAsMemberTesting not working

--- Packages/com.enderlook.unity.toolset/Editor/Testing/CannotBeUsedAsMemberTesting.cs
+++ Packages/com.enderlook.unity.toolset/Editor/Testing/CannotBeUsedAsMemberTesting.cs
@@ -26,7 +26,7 @@ namespace Enderlook.Unity.Toolset.Testing
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by PostCompilingAssembliesHelper")]
         private static void GetFields(FieldInfo fieldInfo)
         {
-            if (fieldInfo.CheckIfShouldBeIgnored(typeof(ShowIfAttribute)))
+            if (fieldInfo.CheckIfShouldBeIgnored(typeof(CannotBeUsedAsMemberAttribute)))
                 return;
             if (types.Contains(fieldInfo.FieldType)) // We do this instead of checking if IsDefined because that method is quite slow (not tested)
                 Debug.LogError($"{fieldInfo.DeclaringType} has a field {fieldInfo.Name} of type {fieldInfo.FieldType} which can not be used as member because it has defined the attribute {nameof(CannotBeUsedAsMemberAttribute)}.");
