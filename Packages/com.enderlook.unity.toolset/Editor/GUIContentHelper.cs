03a44c7bc5a7f8c0afb6b84ba983a67febf2ccf6
Enderlook <enderlook@gmail.com>
2022-07-16 16:20:16 -0300

    Add GetDisplayName and GetTooltip methods in GUIContentHelper

--- Packages/com.enderlook.unity.toolset/Editor/GUIContentHelper.cs
+++ Packages/com.enderlook.unity.toolset/Editor/GUIContentHelper.cs
@@ -3,6 +3,7 @@ using Enderlook.Unity.Toolset.Attributes;
 using Enderlook.Unity.Toolset.Utils;
 
 using System.Reflection;
+using System.Threading;
 
 using UnityEditor;
 
@@ -15,6 +16,8 @@ namespace Enderlook.Unity.Toolset
     /// </summary>
     public static class GUIContentHelper
     {
+        private static GUIContent staticContent;
+
         internal static void UseNameAttribute(NameAttribute attribute, GUIContent label) => label.text = attribute.name;
 
         internal static void UseGUIContent(GUIAttribute attribute, SerializedProperty property, ref GUIContent label)
@@ -64,44 +67,83 @@ namespace Enderlook.Unity.Toolset
             }
         }
 
-        /// <summary>
-        /// Check if the <see cref="SerializedProperty"/> does have a <see cref="GUIAttribute"/> <see cref="System.Attribute"/> and if has change <paramref name="label"/> by its <see cref="GUIContent"/>.
-        /// </summary>
-        /// <param name="property">Serialized property whose label is being modified.</param>
-        /// <param name="label">Current <see cref="GUIContent"/>.</param>
-        /// <returns>Whenever there was or not an special <see cref="GUIContent"/>.</returns>
-        internal static bool GetGUIContent(SerializedProperty property, ref GUIContent label)
+        private static void SetGUIContent(SerializedProperty property, ref GUIContent content)
         {
-            bool isSpecial = false;
-            MemberInfo memberInfo = property.GetMemberInfo();
+            content.text = property.displayName;
+            content.tooltip = property.tooltip;
 
+            MemberInfo memberInfo = property.GetMemberInfo();
             NameAttribute nameAttribute = memberInfo.GetCustomAttribute<NameAttribute>(true);
+            GUIAttribute guiAttribute = memberInfo.GetCustomAttribute<GUIAttribute>(true);
+
             if (!(nameAttribute is null))
             {
-                UseNameAttribute(nameAttribute, label);
-                isSpecial = true;
+                if (!(guiAttribute is null))
+                {
+                    if (nameAttribute.order >= guiAttribute.order)
+                    {
+                        UseNameAttribute(nameAttribute, content);
+                        UseGUIContent(guiAttribute, property, ref content);
+                    }
+                    else
+                    {
+                        UseGUIContent(guiAttribute, property, ref content);
+                        UseNameAttribute(nameAttribute, content);
+                    }
+                }
+                else
+                    UseNameAttribute(nameAttribute, content);
             }
-
-            GUIAttribute guiAttribute = memberInfo.GetCustomAttribute<GUIAttribute>(true);
-            if (!(guiAttribute is null))
-            {
-                UseGUIContent(guiAttribute, property, ref label);
-                isSpecial = true;
-            }
-
-            return isSpecial;
+            else if (!(guiAttribute is null))
+                UseGUIContent(guiAttribute, property, ref content);
         }
 
         /// <summary>
-        /// Produce a <see cref="GUIContent"/> with the <see cref="SerializedProperty.displayName"/> as <see cref="GUIContent.text"/> and <see cref="SerializedProperty.tooltip"/> as <see cref="GUIContent.tooltip"/>.
+        /// Produce a <see cref="GUIContent"/> with label of <paramref name="serializedProperty"/>.
+        /// </summary>
+        /// <param name="serializedProperty">Property to get its <see cref="GUIContent"/>.</param>
+        /// <returns><see cref="GUIContent"/> of <see cref="property"/>.</returns>
+        public static GUIContent GetGUIContent(this SerializedProperty serializedProperty)
+        {
+            GUIContent content = new GUIContent(serializedProperty.name, serializedProperty.tooltip);
+            SetGUIContent(serializedProperty, ref content);
+            return content;
+        }
+
+        /// <summary>
+        /// Get the display name of <paramref name="serializedProperty"/>.
         /// </summary>
         /// <param name="property">Property to get its <see cref="GUIContent"/>.</param>
-        /// <returns><see cref="GUIContent"/> of <see cref="serializedProperty"/>.</returns>
-        public static GUIContent GetGUIContent(this SerializedProperty property)
+        /// <returns>Display name of <see cref="serializedProperty"/>.</returns>
+        public static string GetDisplayName(this SerializedProperty property)
         {
-            GUIContent guiContent = new GUIContent(property.displayName, property.tooltip);
-            GetGUIContent(property, ref guiContent);
-            return guiContent;
+            GUIContent content = Interlocked.Exchange(ref staticContent, null) ?? new GUIContent();
+            SetGUIContent(property, ref content);
+            string name = content.text;
+            content.text = null;
+            content.tooltip = null;
+            staticContent = content;
+            return name;
+        }
+
+        /// <summary>
+        /// Get the tooltip of <paramref name="serializedProperty"/>.
+        /// </summary>
+        /// <param name="property">Property to get its <see cref="GUIContent"/>.</param>
+        /// <returns>Tooltip of <see cref="serializedProperty"/>.</returns>
+        public static string GetTooltip(this SerializedProperty property)
+        {
+            if (property.GetMemberInfo().GetCustomAttribute<GUIAttribute>() is GUIAttribute attribute)
+            {
+                GUIContent content = Interlocked.Exchange(ref staticContent, null) ?? new GUIContent();
+                UseGUIContent(attribute, property, ref content);
+                string tooltip = content.tooltip;
+                content.text = null;
+                content.tooltip = null;
+                staticContent = content;
+                return tooltip;
+            }
+            return property.tooltip;
         }
     }
 }
