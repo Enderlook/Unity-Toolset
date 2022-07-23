781ee207c7d4ac3d3e8a3bfd67203004cbfc586d
Enderlook <enderlook@gmail.com>
2022-07-23 16:50:42 -0300

    Add overloard to ExecuteOnEachFieldOfEachTypeWhenCheckAttribute's constructor

--- Packages/com.enderlook.unity.toolset.checking/Editor/PostCompiling/Attributes/ExecuteOnEachFieldOfEachTypeWhenCheckAttribute.cs
+++ Packages/com.enderlook.unity.toolset.checking/Editor/PostCompiling/Attributes/ExecuteOnEachFieldOfEachTypeWhenCheckAttribute.cs
@@ -24,5 +24,12 @@ namespace Enderlook.Unity.Toolset.Checking.PostCompiling.Attributes
         /// <param name="fieldFlags">Whenever it must be Unity able to serialize it or if it does not matter.</param>
         /// <param name="order">In which order will this method be executed.</param>
         public ExecuteOnEachFieldOfEachTypeWhenCheckAttribute(FieldSerialization fieldFlags = FieldSerialization.EitherSerializableOrNotByUnity, int order = 0) : base(order) => fieldFilter = fieldFlags;
+
+        /// <summary>
+        /// Executes the method decorated by this attribute for each field on each <see cref="Type"/> compiled by Unity.<br/>
+        /// The method to decorate must have the signature DoSomething(<see cref="FieldInfo"/>).
+        /// </summary>
+        /// <param name="order">In which order will this method be executed.</param>
+        public ExecuteOnEachFieldOfEachTypeWhenCheckAttribute(int order = 0) : base(order) => fieldFilter = FieldSerialization.EitherSerializableOrNotByUnity;
     }
 }
