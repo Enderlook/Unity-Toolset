51e7439df49221f096e181ead9d813041229b251
Enderlook <enderlook@gmail.com>
2022-07-25 10:53:18 -0300

    Make ModeName field in PropertyPopupAttribute internal

--- Packages/com.enderlook.unity.toolset/Runtime/Attributes/PropertyPopup/PropertyPopupAttribute.cs
+++ Packages/com.enderlook.unity.toolset/Runtime/Attributes/PropertyPopup/PropertyPopupAttribute.cs
@@ -10,7 +10,7 @@ namespace Enderlook.Unity.Toolset.Attributes
         /// <summary>
         /// Name of field used to determine which property must be used.
         /// </summary>
-        public readonly string ModeName;
+        internal readonly string ModeName;
 
         /// <summary>
         /// Enable property popup on the class which is being decorated.
