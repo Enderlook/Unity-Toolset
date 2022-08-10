30ae14190c46a72e7ceaf4dc68c8712a86151ced
Enderlook <enderlook@gmail.com>
2022-08-10 10:23:04 -0300

    Turn ignoreTypes field of DoNotCheckAttribute into internal

--- Packages/com.enderlook.unity.toolset.checking/Runtime/Attributes/DoNotCheckAttribute.cs
+++ Packages/com.enderlook.unity.toolset.checking/Runtime/Attributes/DoNotCheckAttribute.cs
@@ -5,7 +5,7 @@ namespace Enderlook.Unity.Toolset.Checking
     [AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = true)]
     public sealed class DoNotCheckAttribute : Attribute
     {
-        public readonly Type[] ignoreTypes;
+        internal readonly Type[] ignoreTypes;
 
         public DoNotCheckAttribute(params Type[] attributesToNotCheck) => ignoreTypes = attributesToNotCheck;
     }
