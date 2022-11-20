9677ec02b34f885a84bb82fa02ef5ad66ab05640
Enderlook <enderlook@gmail.com>
2022-11-20 17:02:32 -0300

    Fix ExpandableDrawer not working of fields of type UnityEngine.Object

--- Packages/com.enderlook.unity.toolset/Editor/Drawers/ExpandableDrawer.cs
+++ Packages/com.enderlook.unity.toolset/Editor/Drawers/ExpandableDrawer.cs
@@ -135,9 +135,9 @@ namespace Enderlook.Unity.Toolset.Drawers
         protected internal override void OnGUI(Rect position, SerializedProperty property, GUIContent label, bool includeChildren)
         {
             Type type = property.serializedObject.targetObject.GetType();
-            if (!type.IsSubclassOf(typeof(UnityEngine.Object)))
+            if (!typeof(UnityEngine.Object).IsAssignableFrom(type))
             {
-                Debug.LogError($"{nameof(ExpandableAttribute)} can only be used on types subclasses of {nameof(UnityEngine.Object)}. {property.name} from {property.GetParentTargetObject()} (path {property.propertyPath}) is type {type}.");
+                Debug.LogError($"{nameof(ExpandableAttribute)} can only be used on types assignables to {nameof(UnityEngine.Object)}. {property.name} from {property.GetParentTargetObject()} (path {property.propertyPath}) is type {type}.");
                 EditorGUI.PropertyField(position, property, GUIContent.none, true);
                 return;
             }
