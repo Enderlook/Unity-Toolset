abe80051452417a5374ed190aa8abe838d60cc4f
Enderlook <enderlook@gmail.com>
2022-12-03 18:59:38 -0300

    Fix typo in documentation

--- Packages/com.enderlook.unity.toolset/Runtime/Attributes/InitOnlyAttribute.cs
+++ Packages/com.enderlook.unity.toolset/Runtime/Attributes/InitOnlyAttribute.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 namespace Enderlook.Unity.Toolset.Attributes
 {
     /// <summary>
-    /// Makee the drawn serialized property to only be editable when inspector is not playing.
+    /// Make the drawn serialized property to only be editable when inspector is not playing.
     /// </summary>
     [AttributeUsage(AttributeTargets.Field, Inherited = true)]
     [AttributeUsageFieldMustBeSerializableByUnity]
