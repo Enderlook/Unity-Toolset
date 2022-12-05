25cf5e549576ff7ea3c02ea69d15861ef708238a
Enderlook <enderlook@gmail.com>
2022-12-05 09:18:43 -0300

    Refactor RangeDrawer

--- Packages/com.enderlook.unity.toolset/Editor/Drawers/RangeDrawer.cs
+++ Packages/com.enderlook.unity.toolset/Editor/Drawers/RangeDrawer.cs
@@ -7,22 +7,15 @@ namespace Enderlook.Unity.Toolset.Drawers
     [CustomStackablePropertyDrawer(typeof(RangeAttribute))]
     internal sealed class RangeDrawer : StackablePropertyDrawer
     {
-        private bool isMain;
-
         protected internal override bool RequestMain => true;
-
-        protected internal override void IsMain(bool isMain) => this.isMain = isMain;
 
         protected internal override void OnGUI(Rect position, SerializedProperty property, GUIContent label, bool includeChildren)
         {
-            if (isMain)
-            {
-                RangeAttribute attribute = (RangeAttribute)Attribute;
-                if (property.propertyType == SerializedPropertyType.Integer)
-                    EditorGUI.IntSlider(position, property, (int)attribute.min, (int)attribute.max, label);
-                else
-                    EditorGUI.Slider(position, property, attribute.min, attribute.max, label);
-            }
+            RangeAttribute attribute = (RangeAttribute)Attribute;
+            if (property.propertyType == SerializedPropertyType.Integer)
+                EditorGUI.IntSlider(position, property, (int)attribute.min, (int)attribute.max, label);
+            else
+                EditorGUI.Slider(position, property, attribute.min, attribute.max, label);
         }
     }
 }
